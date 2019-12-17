0fd23b58bf15ba8fd5d1c0bfb9a8070c3bf0a4f5
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-17 14:06:41 +0100

    Update install-wordpress.sh
    corrected downloaded tar.gz

--- install-wordpress.sh
+++ install-wordpress.sh
@@ -2,10 +2,10 @@
 yum install php php-mysql -y
 cd /var/www/html
 wget https://wordpress.org/latest.tar.gz
-tar -xzf wordpress-*.tar.gz
+tar -xzf *.tar.gz
 cp -r wordpress/* /var/www/html/
 rm -rf wordpress
-rm -rf wordpress-*.tar.gz
+rm -rf *.tar.gz
 chmod -R 755 wp-content
 chown -R apache:apache wp-content
 service httpd reload
