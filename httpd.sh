eb470fcffcdba5839e031f850a1efc5d14234854
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-17 12:15:23 +0100

    Update httpd.sh
    disabled ipv6 so apache listens always in ipv4

--- httpd.sh
+++ httpd.sh
@@ -2,5 +2,8 @@
 
 yum update -y 
 yum install httpd -y
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+sysctl -p
 systemctl enable httpd
 systemctl start httpd
