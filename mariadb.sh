4bc1370aaa96f4d8218f5ccbd2531a0fcccdd089
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-19 23:12:04 +0100

    Update mariadb.sh
    corrected mysql strings

--- mariadb.sh
+++ mariadb.sh
@@ -8,10 +8,10 @@ yum -y install mariadb-server
 systemctl start mariadb
 systemctl enable mariadb
 mysqladmin -u root password $1
-mysql -uroot -p"$1" -e "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'" > /dev/null 2>&1
-mysql -uroot -p"$1" -e "DELETE FROM mysql.user where User='root' and Host not in ('localhost','127.0.0.1', '::1')" > /dev/null 2>&1
-mysql -uroot -p"$1" -e "DELETE FROM mysql.user where User=''" > /dev/null 2>&1
-mysql -uroot -p"$1" -e "DELETE from mysql.db where db='test' or Db='test\_%'" > /dev/null 2>&1
-mysql -uroot -p"$1" -e "FLUSH PRIVILEGES" > /dev/null 2>&1
+mysql -u root -p"$1" -e "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'" > /dev/null 2>&1
+mysql -u root -p"$1" -e "DELETE FROM mysql.user where User='root' and Host not in ('localhost','127.0.0.1', '::1')" > /dev/null 2>&1
+mysql -u root -p"$1" -e "DELETE FROM mysql.user where User=''" > /dev/null 2>&1
+mysql -u root -p"$1" -e "DELETE from mysql.db where db='test' or Db='test\_%'" > /dev/null 2>&1
+mysql -u root -p"$1" -e "FLUSH PRIVILEGES" > /dev/null 2>&1
 sleep 10s
 
