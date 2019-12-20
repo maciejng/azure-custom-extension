cb156212a4a63ec98cef2e2f394765e3fcd3b30a
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-20 10:07:21 +0100

    Update mysql.sh
    doing mysql_secure_installation programmatically

--- mysql.sh
+++ mysql.sh
@@ -10,5 +10,11 @@ yum update -y
 yum install mysql-server -y
 systemctl start mysqld
 systemctl enable mysqld
+mysqladmin -u root password $1
+mysql -u root -p"$1" -e "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'" > /dev/null 2>&1
+mysql -u root -p"$1" -e "DELETE FROM mysql.user where User='root' and Host not in ('localhost','127.0.0.1', '::1')" > /dev/null 2>&1
+mysql -u root -p"$1" -e "DELETE FROM mysql.user where User=''" > /dev/null 2>&1
+mysql -u root -p"$1" -e "DELETE from mysql.db where db='test' or Db='test\_%'" > /dev/null 2>&1
+mysql -u root -p"$1" -e "FLUSH PRIVILEGES" > /dev/null 2>&1
 sleep 10s
 
