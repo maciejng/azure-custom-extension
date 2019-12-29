f5aedc52b000e135f41ff0d78dc496607e819850
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-29 14:41:26 +0100

    Add files via upload
    postgresql connector for wordpress

---
+++ pg4wp.sh
@@ -0,0 +1,5 @@
+cd wp-content // go to wp-content
+git clone https://github.com/kevinoid/postgresql-for-wordpress.git
+mv postgresql-for-wordpress/pg4wp Pg4wp
+rm -rf postgresql-for-wordpress
+cp Pg4wp/db.php db.php
