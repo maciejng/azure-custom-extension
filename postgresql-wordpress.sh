673833ed4427b738661b957075615b7e47920ea3
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-29 18:19:44 +0100

    Update postgresql-wordpress.sh
    typos corrected

--- postgresql-wordpress.sh
+++ postgresql-wordpress.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo su - postgres -c createdb $1
-sudo -u postgres bash -c "psql -c \"CREATE USER $2 WITH PASSWORD '$3';\""
-sudo -u postgres bash -c "psql -c \"grant all privileges on database $1 to $2\""
-
+sudo su - postgres -c "createdb $1"
+sudo su - postgres -c "psql -c \"CREATE USER $2 WITH PASSWORD '$3';\""
+sudo su - postgres -c "psql -c \"grant all privileges on database $1 to $2\""
