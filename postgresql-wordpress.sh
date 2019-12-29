9af25afae95a80927cd395f7731d211bbabb3548
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-29 20:29:39 +0100

    Update postgresql-wordpress.sh
    $4 was already reserved

--- postgresql-wordpress.sh
+++ postgresql-wordpress.sh
@@ -3,5 +3,5 @@ sudo su - postgres -c "createdb $1"
 sudo su - postgres -c "psql -c \"CREATE USER $2 WITH PASSWORD '$3';\""
 sudo su - postgres -c "psql -c \"grant all privileges on database $1 to $2\""
 echo "listen_addresses='*'" >> vi /var/lib/pgsql/data/postgresql.conf
-echo "host all all $4 trust" >> vi /var/lib/pgsql/data/pg_hba.conf
+echo "host all all $5 trust" >> vi /var/lib/pgsql/data/pg_hba.conf
 service postgresql restart
