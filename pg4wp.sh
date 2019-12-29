f0e25d3a111f07335f234b76a091da23df6e0d54
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-29 21:07:09 +0100

    Update pg4wp.sh
    bug in path after last commit

--- pg4wp.sh
+++ pg4wp.sh
@@ -2,6 +2,6 @@ yum install git php-pgsql -y
 git clone https://github.com/kevinoid/postgresql-for-wordpress.git
 mv postgresql-for-wordpress/pg4wp /var/www/html/wp-content/pg4wp
 rm -rf postgresql-for-wordpress
-cp /var/www/html/wp-content/Pg4wp/db.php /var/www/html/wp-content/db.php
+cp /var/www/html/wp-content/pg4wp/db.php /var/www/html/wp-content/db.php
 chown apache. /var/www/html/wp-content/* -R
 service httpd restart
