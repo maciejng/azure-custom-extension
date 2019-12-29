cc29f98f455418e1395f3ab7df4701edce0da189
maciejng <58933028+maciejng@users.noreply.github.com>
2019-12-29 20:51:05 +0100

    Update postgresql-wordpress.sh
    paths corrected

--- postgresql-wordpress.sh
+++ postgresql-wordpress.sh
@@ -2,6 +2,6 @@
 sudo su - postgres -c "createdb $1"
 sudo su - postgres -c "psql -c \"CREATE USER $2 WITH PASSWORD '$3';\""
 sudo su - postgres -c "psql -c \"grant all privileges on database $1 to $2\""
-echo "listen_addresses='*'" >> vi /var/lib/pgsql/data/postgresql.conf
-echo "host all all $5 trust" >> vi /var/lib/pgsql/data/pg_hba.conf
+echo "listen_addresses='*'" >> /var/lib/pgsql/data/postgresql.conf
+echo "host all all $5 trust" >> /var/lib/pgsql/data/pg_hba.conf
 service postgresql restart
